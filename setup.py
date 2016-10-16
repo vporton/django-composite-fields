510553cbf5a0a3ca9b82d9af615b648b60a36367
Victor Porton <porton@narod.ru>
2016-10-16 16:41:14 +0300

    license

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     author_email='porton@narod.ru',
 
     # Choose your license
-    license='Closed',
+    license='BSD license',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -40,7 +40,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: BSD License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
