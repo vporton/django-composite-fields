6c3dad730ba4d305d0fbba2e86d180f5ae02bd1c
Victor Porton <porton@narod.ru>
2016-10-16 17:30:11 +0300

    test models moved into models.py and now tests run without errors

--- tests/test.py
+++ tests/test.py
@@ -1,16 +1,5 @@
 from django.test import TestCase
-from composite_fields.composite_fields import *
-
-
-class MyCompositeField(CompositeField):
-    field1 = models.fields.IntegerField()
-    field2 = models.fields.IntegerField()
-
-
-class MyModel(ModelWithCompositeFields):
-    composite1 = MyCompositeField(name='composite1')
-    composite2 = MyCompositeField(name='composite2')
-
+from .models import *
 
 class BaseUserGroupTestCase(TestCase):
     def setUp(self):
